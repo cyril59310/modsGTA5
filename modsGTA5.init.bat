6555c03cb880ca2c0c12604edd984cf13d7d2af3
cyril59310 <archas.cyril@hotmail.fr>
2021-08-15 03:17:07 +0200

    ajout d'une verification de la connexion a internet

--- modsGTA5.init.bat
+++ modsGTA5.init.bat
@@ -1,5 +1,17 @@
 @echo off
 color 3
+:deb
+cls
+echo.
+title Connexion a internet...
+echo Teste de la connexion a internet...
+ping google.fr -n 2 >nul
+if %ERRORLEVEL%==1 (
+	echo Aucune connexion a internet...
+	echo mise a jours impossible
+	pause
+	goto deb
+)
 title Download...
 echo Telechargement du code...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://cyril59310.fr/download/dev/code-modsGTA5', 'modsGTA5.bat')"
